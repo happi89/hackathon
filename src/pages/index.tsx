103869d4151b9754d11c54703d780c8b0c36d260
happi89 <farhaanp99@gmail.com>
2022-12-02 15:23:02 -0500

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from 'next';
+import { Text } from '@mantine/core';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Text>Hi</Text>
+		</>
+	);
+};
+
+export default Home;
